ac936bd771dd63f3a52781d60468b9cde74387a3
Shivam Mathur <shivam_jpr@hotmail.com>
2021-03-17 08:46:13 +0530

    Improve source extension setup logs
    Refactor source extension setup code to src/scripts/ext/source.sh

--- src/scripts/darwin.sh
+++ src/scripts/darwin.sh
@@ -212,6 +212,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 
 # shellcheck source=.
+. "${scripts:?}"/ext/source.sh
 . "${scripts:?}"/tools/add_tools.sh
 . "${scripts:?}"/common.sh
 read_env
